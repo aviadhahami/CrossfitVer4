563547d55edf14b4bff7418864711a565a274385
aviadhahami <aviadhahami@gmail.com>
2014-09-28 22:44:32 +0300

    FUCKING REVERT JESUS CHIRST

--- wpbootstrap/header.php
+++ wpbootstrap/header.php
@@ -1,82 +1,40 @@
-<head>
+  <head>
     <meta charset="utf-8">
     <title>Bootstrap, from Twitter</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <?php wp_enqueue_script( "jquery"); ?>
     <!-- Le styles -->
     <link href="<?php bloginfo('stylesheet_url');?>" rel="stylesheet">
 
-
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
-
-    <!-- Optional theme -->
-
-    <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css"> -->
-    <script src="//code.jquery.com/jquery-1.11.1.js"></script>
-    <!-- Latest compiled and minified JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 
     <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
     <!--[if lt IE 9]>
       <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
     <![endif]-->
 
+    <?php wp_enqueue_script("jquery"); ?>
     <?php wp_head(); ?>
-</head>
+  </head>
+  <body>
 
-<body>
+  <div class="navbar navbar-inverse navbar-fixed-top">
+    <div class="navbar-inner">
+      <div class="container">
+        <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </a>
+        <a class="brand" href="<?php echo site_url(); ?>"><?php bloginfo('name'); ?></a>
+        <div class="nav-collapse collapse">
+          <ul class="nav">
 
+              <?php wp_list_pages(array('title_li' => '', 'exclude' => 4)); ?>
 
-    <div class="navbar navbar-inverse navbar-fixed-top">
-        <div class="navbar-inner">
-            <div class="container">
-                <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </a>
-                <a class="brand" href="<?php echo site_url(); ?>">
-                    <?php bloginfo( 'name'); ?>
-                </a>
-                <div class="nav-collapse collapse">
-                    <ul class="nav">
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </div>
+  </div>
 
-                        <?php wp_list_pages(array( 'title_li'=>'', 'exclude' => 4)); ?>
-
-                    </ul>
-                </div>
-                <!--/.nav-collapse -->
-                <<<<<<< HEAD </div>
-            </div>
-        </div>
-
-        <div class="container">
-
-
-            <!-- Navigation -->
-            <div class="navbar navbar-custom navbar-fixed-top" role="navigation">
-                <div class="container-fluid">
-                    <div class="navbar-header">
-                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse">
-                            <i class="fa fa-bars"></i>
-                        </button>
-                        <a class="navbar-brand page-scroll" href="#page-top">
-                            <img src="/wp-content/themes/wpbootstrap/img/logo_white.png" class="logo_pic" />
-                        </a>
-                    </div>
-
-                    <!-- Collect the nav links, forms, and other content for toggling -->
-                    <div class="collapse navbar-collapse navbar-right navbar-main-collapse">
-                        <ul class="nav navbar-nav">
-                            <!-- Hidden li included to remove active class from about link when scrolled up past about section -->
-                            <li class="hidden">
-                                <?php wp_list_pages(array( 'title_li'=>'', 'exclude' => 4)); ?>
-                            </li>
-                        </ul>
-                    </div>
-                    <!-- /.navbar-collapse -->
-                </div>
-                <!-- /.container -->
-            </div>
+  <div class="container">
