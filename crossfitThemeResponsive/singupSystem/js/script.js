956997793cf94214e96f17b4a1bf759d7f886a2c
aviadhahami <aviadhahami@gmail.com>
2014-10-18 16:45:35 +0300

    SignUpSystem form validation done

--- crossfitThemeResponsive/singupSystem/js/script.js
+++ crossfitThemeResponsive/singupSystem/js/script.js
@@ -2,22 +2,27 @@ $(document).ready(function() {
     var cellCounter = 2;
     /* BUTTON HANDLERS */
     $("#submit").on("click", function() {
-        alert("SUBMITTED FORM");
+        if (submitValidation()) {
+            $("#response").text("הטופס מולא בהצלחה");
+        } else {
+            $("#response").text("קיימות שגיאות בטופס");
+        }
     });
     $("#reset").on("click", function() {
-        $("table").find("tr:gt(1)").remove();
+        $("#mainTable").find("tr:gt(1)").remove();
+        $("#mainTable").find("tr td input").val("");
         cellCounter = 2;
         console.log("cellCounter = " + cellCounter);
     });
     $("#appendRow").on("click", function() {
         var tableRow = '<tr><td><input type="text" /></td><td><input type="text" /></td><td><input type="text" /></td><td><p>' + cellCounter + '</p></td></tr>';
-        $("table tr:last").after(tableRow);
+        $("#mainTable tr:last").after(tableRow);
         cellCounter++;
         console.log("cellCounter = " + cellCounter);
     });
     $("#removeRow").on("click", function() {
         if (cellCounter > 1) {
-            $("table").find("tr:last").remove();
+            $("#mainTable").find("tr:last").remove();
             cellCounter--;
         }
         console.log("cellCounter = " + cellCounter);
@@ -30,4 +35,23 @@ $(document).ready(function() {
 
         });
     });
+
+    /* FUNCTIONS AREA */
+    function submitValidation() {
+        var flag = true;
+        var date = $("#datepicker").val();
+        if ( !! date != false) {
+            $("#mainTable tr").find("td input").each(function() {
+                console.log($(this).val());
+                if ($(this).val().length == 0) {
+                    console.log("field is empty");
+                    console.log("RETURNING FALSE");
+                    flag = false;
+                }
+            });
+        } else {
+            flag = false;
+        }
+        return flag;
+    }
 });
