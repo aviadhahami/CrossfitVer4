6d65be801487db1f67eff42ad8894b1e3874977c
aviadhahami <aviadhahami@gmail.com>
2014-10-29 11:36:07 +0200

    updated a lot of things - full rework

--- crossfitThemeResponsive/singupSystem/js/script.js
+++ crossfitThemeResponsive/singupSystem/js/script.js
@@ -1,102 +1,25 @@
 $(document).ready(function() {
-    var cellCounter = 2;
-    /* BUTTON HANDLERS */
+
+    $("#datepicker").datepicker({
+        dateFormat: "dd-mm-y"
+
+    });
+    //APPEND A LOT OF ROWS
+    for (var i = 0; i < 30; i++) {
+        $("#mainTable tbody").append("<tr><td><input type='text' name='limit" + i + "'></td><td><input type='text' name='wod" + i + "'></td></tr>");
+    }
+
+    //SHOW VALUES FROM INPUT
     $("#submit").on("click", function() {
-        if (submitValidation()) {
-            $("#response").text("הטופס מולא בהצלחה");
-        } else {
-            $("#response").text("קיימות שגיאות בטופס");
-        }
-    });
-    $("#reset").on("click", function() {
-        $("#mainTable").find("tr:gt(1)").remove();
-        $("#mainTable").find("tr td input").val("");
-        cellCounter = 2;
-        console.log("cellCounter = " + cellCounter);
-    });
-    $("#appendRow").on("click", function() {
-        var tableRow = '<tr><td><input type="text" /></td><td><input type="text" /></td><td><input type="text" /></td><td><p>' + cellCounter + '</p></td></tr>';
-        $("#mainTable tr:last").after(tableRow);
-        cellCounter++;
-        console.log("cellCounter = " + cellCounter);
-    });
-    $("#removeRow").on("click", function() {
-        if (cellCounter > 1) {
-            $("#mainTable").find("tr:last").remove();
-            cellCounter--;
-        }
-        console.log("cellCounter = " + cellCounter);
-    });
-
-    //DATE PICKER
-    $(function() {
-        $("#datepicker").datepicker({
-            dateFormat: 'dd/mm/y',
-            onClose: function() {
-                appender($(this).val());
+        $("#mainTable tbody tr td input").each(function() {
+            //if value is not empty
+            if ( !! $(this).val()) {
+                console.log($(this).val() + " and " + $(this).attr("name"));
             }
 
         });
-
     });
 
-    /* FUNCTIONS AREA */
-    function submitValidation() {
-        var flag = true;
-        var date = $("#datepicker").val();
-        if ( !! date != false) {
-            $("#mainTable tr").find("td input").each(function() {
-                console.log($(this).val());
-                if ($(this).val().length == 0) {
-                    console.log("field is empty");
-                    console.log("RETURNING FALSE");
-                    flag = false;
-                }
-            });
-        } else {
-            flag = false;
-        }
-        return flag;
-    }
-
-
-
-
-    //POLL DATA INJETION
-    //insert the daily sched
-    function appender(date) {
-        date = date.replace(/\//g, '');
-        var url = "overrides/" + date + "-Override.xml";
-        console.log(url);
-        $("#response").text("");
-        $("#poll").empty();
-        $.ajax({
-            type: "GET",
-            url: url,
-            datatype: "xml",
-            statusCode: {
-                404: function() {
-                    $("#response").text("אין קובץ העוקף את הלו''ז המקורי");
-                }
-            },
-            success: function(xml) {
-                console.log(xml);
-                $(xml).find("data option").each(function() {
-                    var val = $(this).find("value").text();
-                    var rawData = $(this).find("rawData").text();
-                    var option = "<option val=" + val + ">" + rawData + "</option>";
-                    console.log(option);
-                    //option.appendTo("#poll");
-                    $("#poll").append(option);
-                });
-
-            },
-            error: function(err) {
-                console.error('ERROR');
-                console.log(err.statusText);
-            }
-        });
-    }
 
 
     //** LEGACY CODE **//
